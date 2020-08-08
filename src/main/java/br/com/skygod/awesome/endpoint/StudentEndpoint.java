3d1e161211380a8100c77c9399b07a6649690e7d
SkyG0D <silvanosilvino@hotmail.com>
2020-08-07 23:29:23 -0300

    Estudos do dia 7 de agosto.

--- src/main/java/br/com/skygod/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/skygod/awesome/endpoint/StudentEndpoint.java
@@ -1,34 +1,44 @@
 package br.com.skygod.awesome.endpoint;
 
+import br.com.skygod.awesome.error.CustomErrorType;
 import br.com.skygod.awesome.model.Student;
 import br.com.skygod.awesome.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentEndpoint {
 
+    private final DateUtil dateUtil;
+
     @Autowired
-    private DateUtil dateUtil;
+    public StudentEndpoint(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/list")
-    public List<Student> listAll() {
-        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(
-                LocalDateTime.now()
-        ));
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> listAll() {
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+    }
 
-        return Arrays.asList(
-                new Student("Silvano"),
-                new Student("Gustavo"),
-                new Student("Ademir")
-        );
+    @RequestMapping(method = RequestMethod.GET, path="/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+        if (index >= 0) return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+
+        return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
     }
 
 }
