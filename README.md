3a8ead281360b9f2fb03dcfa93047289442e6330
SkyG0D <silvanosilvino@hotmail.com>
2020-08-07 21:01:07 -0300

    Primeiro commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+#Curso de Spring Boot
+Curso de springboot disponibilizado pelo [DevDojo](https://www.youtube.com/channel/UCjF0OccBT05WxsJb2zNkL4g).
+
+Repositório criado para estudo.
---
+++ src/main/java/br/com/skygod/awesome/endpoint/StudentEndpoint.java
@@ -0,0 +1,34 @@
+package br.com.skygod.awesome.endpoint;
+
+import br.com.skygod.awesome.model.Student;
+import br.com.skygod.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndpoint {
+
+    @Autowired
+    private DateUtil dateUtil;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listAll() {
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(
+                LocalDateTime.now()
+        ));
+
+        return Arrays.asList(
+                new Student("Silvano"),
+                new Student("Gustavo"),
+                new Student("Ademir")
+        );
+    }
+
+}
